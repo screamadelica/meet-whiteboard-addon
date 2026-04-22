96c2d946f7ca710099330c90291fe432897842ab
screamadelica <97456834+screamadelica@users.noreply.github.com>
2026-04-22 10:08:29 +0300

    adding throttling

--- src/excalidraw-bundle.js
+++ src/excalidraw-bundle.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import * as ExcalidrawLib from "@excalidraw/excalidraw";
 import { Peer } from "peerjs";
+import throttle from "lodash.throttle";
 
 // Must be assigned to globalThis, not just window, for IIFE scope
 globalThis.process = { env: { NODE_ENV: "production" } };
@@ -9,3 +10,4 @@ globalThis.React = React;
 globalThis.ReactDOM = ReactDOM;
 globalThis.ExcalidrawLib = ExcalidrawLib;
 globalThis.Peer = Peer;
+window.throttle = throttle;
