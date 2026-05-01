9636bca62f1820820e375cf8302fa74fbb247ec1
screamadelica <97456834+screamadelica@users.noreply.github.com>
2026-05-01 09:54:48 +0300

    adding default turn servers

--- api/get-ice-servers.js
+++ api/get-ice-servers.js
@@ -1,5 +1,6 @@
 export default async function handler(request, response) {
     try {
+        console.log("[API] get-ice-servers called")
         // 1. Safe attempt to import and initialize Turnix
         let turnix;
         try {
@@ -27,9 +28,24 @@ export default async function handler(request, response) {
         
         return response.status(200).json({
             config: {
-                iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
-                iceTransportPolicy: 'all'
-            }
+                "iceServers":[
+                {
+                    "urls":["stun:stun.turnix.io:3478"]
+
+                },
+                {
+                    "username":"7e6ffbb6-44ca-4aea-97a4-e895d4831b46",
+                    "credential":"b392642eb83cf9454bb3fd1af80eedee",
+                    "urls":[
+                        "turn:eu-central.turnix.io:3478?transport=udp",
+                        "turn:eu-central.turnix.io:3478?transport=tcp",
+                        "turns:eu-central.turnix.io:443?transport=udp",
+                        "turns:eu-central.turnix.io:443?transport=tcp"]
+                    }
+                ],
+                "expiresAt":"2026-05-31T06:48:36.283Z"
+            },
+            iceTransportPolicy: 'all'
         });
     }
 }
